58eded47f13873faaf723eb1b989a9efbf7c08b2
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-03-02 21:10:27 +0400

    added Configure Store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import configureStore from './redux/reducers/configureStore';
+import {Provider} from "react-redux";
+
+const store=configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <Provider store={store} > <App /></Provider>
+ </React.StrictMode>,
   document.getElementById('root')
 );
 
